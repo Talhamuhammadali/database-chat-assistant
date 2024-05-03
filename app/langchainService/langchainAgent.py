5d2a6e7eaa78d492a748634a6ddd49aa185cf148
Talhamuhammadali <talha_muhammadali@hotmail.com>
2024-05-03 15:36:26 +0500

    Fix: vllm usage fixed

--- app/langchainService/langchainAgent.py
+++ app/langchainService/langchainAgent.py
@@ -4,14 +4,10 @@ logging.basicConfig(level="INFO")
 logger = logging.getLogger("langchain_service")
 
 chat_llm = VLLM(
-    openai_api_key="EMPTY",
-    openai_api_base="http://localhost:8000/v1/",
-    model_name="lama3_chat_instruct"
+    model="lama3_chat_instruct"
 )
 sql_llm = VLLM(
-    openai_api_key="EMPTY",
-    openai_api_base="http://localhost:8001/v1/",
-    model_name="phi_2_finetuined"
+    model="phi_2_finetuined"
 )
 def test():
     logger.info("Testing langchain")
